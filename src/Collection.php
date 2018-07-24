3a32d59590169ba09d34291f2297a36c2c32648f
Payton Bice <hellopayton@gmail.com>
2018-07-23 20:04:00 -0500

    Adds methods for registering GET, POST, and wildcard routes

--- src/Collection.php
+++ src/Collection.php
@@ -50,6 +50,18 @@ class Collection {
         return $this;
     }
 
+    public function get(string $route, callable $handler): self {
+        return $this->register($route, 'get', $handler);
+    }
+
+    public function post(string $route, callable $handler): self {
+        return $this->register($route, 'post', $handler);
+    }
+
+    public function all(string $route, callable $handler): self {
+        return $this->register($route, 'all', $handler);
+    }
+
     public function match(string $route) {
         if ($route !== '/') {
             $route = trim($route, '/');
